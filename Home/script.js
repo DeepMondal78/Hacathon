ce8b4d8bb4d4aae58b89653a6209587db8b3f6a9
Deep Mondal <deepmondalkumar6@gmail.com>
2025-04-10 14:38:38 +0530

    update

--- Home/script.js
+++ Home/script.js
@@ -71,3 +71,17 @@ prevBtn.addEventListener("click", () => {
     currentIndex = (currentIndex - 1 + watchImages.length) % watchImages.length;
     watchImg.src = watchImages[currentIndex];
 });
+
+// ==== CircleText Animetion ====
+const text = document.getElementById("image-text");
+const content = "• APPLE WATCH • SERIES 6 • 44MM ALUMINUM & CERAMIC CASE • ION-X GLASS • GPS • LTE • WR-50M •";
+
+const chars = content.split("");
+const angle = 360 / chars.length; // Auto spacing
+
+text.innerHTML = chars
+  .map(
+    (char, i) =>
+      `<span class="char" style="transform: rotate(${i * angle}deg);">${char}</span>`
+  )
+  .join("");
